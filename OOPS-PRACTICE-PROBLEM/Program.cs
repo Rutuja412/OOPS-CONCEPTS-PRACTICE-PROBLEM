691edbde14e522e15ef2e5859c0c2edc0b7cee96
Rutuja412 <rutujakadam4545@gmail.com>
2023-06-15 23:23:37 +0530

    [Rutuja]:Variables Example

--- OOPS-PRACTICE-PROBLEM/Program.cs
+++ OOPS-PRACTICE-PROBLEM/Program.cs
@@ -9,7 +9,7 @@ namespace OOPS_PRACTICE_PROBLEM
         {
             Console.WriteLine("Welcome to OOPS Concept");
             Console.WriteLine("Select given options");
-            Console.WriteLine("1.Class &Objects \n2.Inheritance(Vehicle)\n3.Polymorphism - Method Overloding\n4.Overriding(poly)\n5 Abstraction  ");
+            Console.WriteLine("1.Class &Objects \n2.Inheritance(Vehicle)\n3.Polymorphism - Method Overloding\n4.Overriding(poly)\n5 Abstraction \n6.Variables  ");
             int option = Convert.ToInt16(Console.ReadLine());
             switch (option)
             {
@@ -39,6 +39,23 @@ namespace OOPS_PRACTICE_PROBLEM
                     Cat cat = new Cat();
                     cat.AnimalSound();
                     cat.Eat();
+                    break;
+
+                case 6:
+                    Console.WriteLine("Variables Types");
+                    Variables variables = new Variables();
+                    variables.EmployeeAge();//Local
+                    Variables.Salary = 10000;//Static
+                    Console.WriteLine("Employee Salary =" + Variables.Salary);
+                    Console.WriteLine("Employee Salary ; " + Variables.Monthlysalary);//Const variable
+                    Console.WriteLine("Employee LastName;" + variables.LastName);//readomnly
+                    variables.EmployeeName = "Rutuja";//Instance 
+                    variables.EmployeeId = 1;
+                    Console.WriteLine("Employee Name= " + variables.EmployeeName + " \nEmployee ID; " + variables.EmployeeId);
+                    Variables variables1 = new Variables();
+                    variables1.EmployeeName = "Radha";//Instance Second Object
+                    variables1.EmployeeId = 2;
+                    Console.WriteLine("Employee Name= " + variables1.EmployeeName + " \nEmployee ID; " + variables1.EmployeeId);
                     break;
 
 
---
+++ OOPS-PRACTICE-PROBLEM/Variables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS_PRACTICE_PROBLEM
+{
+    public class Variables
+    {
+        public static double Salary;//static variables
+        public static string Name = "Riya";
+        public string EmployeeName;//Instance or Non Static Variables
+        public int EmployeeId;
+        public const float Monthlysalary = 20000;
+        public readonly string LastName;//ReadOnly
+        public Variables()
+        {
+            this.LastName = "Kadam";
+        }
+        public void EmployeeAge()
+        {
+            int age = 20;//Local Variables
+            Console.WriteLine(" Marks of Student :" + age);
+
+        }
+    }
+}
