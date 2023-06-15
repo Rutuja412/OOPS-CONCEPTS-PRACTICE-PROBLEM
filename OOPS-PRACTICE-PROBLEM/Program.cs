8f9e0f79e33f9fd35854f1d1adccab6363564221
Rutuja412 <rutujakadam4545@gmail.com>
2023-06-15 22:28:54 +0530

    [Rutuja]:Welcome

---
+++ OOPS-PRACTICE-PROBLEM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPS_PRACTICE_PROBLEM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to OOPS Concept");
+        }
+    }
+}
