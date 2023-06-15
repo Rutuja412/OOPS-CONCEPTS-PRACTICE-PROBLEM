45c99e3198d227b3e726183b82db85e1d170efd7
Rutuja412 <rutujakadam4545@gmail.com>
2023-06-15 22:45:40 +0530

    [Rutuja]:Inheritance Example

--- OOPS-PRACTICE-PROBLEM/Program.cs
+++ OOPS-PRACTICE-PROBLEM/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.ConstrainedExecution;
 
 namespace OOPS_PRACTICE_PROBLEM
 {
@@ -7,8 +8,25 @@ namespace OOPS_PRACTICE_PROBLEM
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to OOPS Concept");
-            ClassAndObjects objects = new ClassAndObjects();
-            objects.CreateClassAndObject();
+            Console.WriteLine("Select given options");
+            Console.WriteLine("1.Class &Objects \n2.Inheritance(Vehicle)\n3.Exit ");
+            int option = Convert.ToInt16(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Class & Objects");
+                    ClassAndObjects objects = new ClassAndObjects();
+                    objects.CreateClassAndObject();
+                    break;
+                case 2:
+                    Console.WriteLine("Inheritance ..( Ex.Vehicle)");
+                    Car car = new Car();
+                    car.horn();
+                    Console.WriteLine("Color:" + car.color + " \nBrand:" + car.brand);
+                    break;
+
+            }
+
         }
     }
 }
---
+++ OOPS-PRACTICE-PROBLEM/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS_PRACTICE_PROBLEM
+{
+    public class Vehicle
+    {
+        public string brand = "Ford";
+        public void horn()
+        {
+            Console.WriteLine("Peek Peek");
+        }
+    }
+    class Car : Vehicle
+    {
+        public string color = "Red";
+    }
+}
